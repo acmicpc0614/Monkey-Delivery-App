e2af82c920f67707fab24a25bbfe0e13928cf093
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-06-27 16:12:12 +0100

    perf food page -YJU

--- src/components/Menu/FoodSlide.js
+++ src/components/Menu/FoodSlide.js
@@ -4,14 +4,14 @@ const FoodSlide = ({ img, description,rate,dessertName}) => {
     <>
       <div className="mb-1 flex flex-row items-end">
         <div className="absolute mb-4" >
-          <div className="px-5 text-[16px] font-bold text-white">
+          <div className="px-5 text-base sm:text-3xl  font-bold text-white">
             {description}
           </div>
           <div className="flex flex-row items-center ">
-            <div className="text-[11px] text-[#FC6011] flex flex-row ml-5 mr-1"><img src={star} className="mr-1"/>{rate}</div> 
-            <div className="text-white text-[12px] mr-3">{dessertName}</div>  
-            <div className="text-[#FC6011] text-[16px] mb-2">.</div>          
-            <div className="text-white text-[12px]">Desserts</div>   
+            <div className="text-xs sm:text-base text-[#FC6011] flex flex-row ml-5 mr-1"><img src={star} className="mr-1"/>{rate}</div> 
+            <div className="text-white text-xs sm:text-base mr-3">{dessertName}</div>  
+            <div className="text-[#FC6011] text-base mb-2">.</div>          
+            <div className="text-white text-xs sm:text-base">Desserts</div>   
           </div>
         </div>
         <img src={img} className="w-full " />
