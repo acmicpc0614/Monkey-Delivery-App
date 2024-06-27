fbd8e102aa511469c9a4e26070dbb39d3c943fab
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-06-27 10:04:37 +0100

    menu by YJU

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,5 +1,14 @@
+import shopCart from "./images/shopping-cart.svg"
 const Menu = () => {
-  return <>Menu</>;
+  return <>
+    <div className="w-full px-5">
+      <div className="flex flex-row">
+        <p className="text-2xl font-[#4A4B4D]">Menu</p>
+        <img src={shopCart}/>
+      </div>
+      
+    </div>
+  </>;
 };
 
 export default Menu;
