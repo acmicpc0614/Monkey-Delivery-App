bf3b987f9a05b66ba6418fcb39dafef10df6b335
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-06-27 12:20:42 +0100

    complete Menu page by YJU

--- src/pages/Menu/Food.js
+++ src/pages/Menu/Food.js
@@ -1,5 +1,22 @@
+import shopCart from "./images/shopping-cart.svg";
+import searchIcon from "./images/search-icon.svg";
 const Food = () => {
-  return <>Food</>;
+  return <>
+   <div className="w-full px-5">
+        <div className="flex flex-row justify-between items-center mt-8">
+          <p className="text-2xl text-[#4A4B4D]">Menu</p>
+          <img src={shopCart} />
+        </div>
+
+        <div className="w-full mt-6">
+          <img src={searchIcon} className="mt-3 ml-5 absolute z-10" />
+          <input
+            className="w-full h-11 bg-[#F2F2F2] rounded-full indent-14 outline-none"
+            placeholder="Search food"
+          ></input>
+        </div>     
+      </div>
+      </>;
 };
 
 export default Food;
