dc97cdd9dadb539c3f3ff778eebb1463d9dda003
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-06-27 11:50:02 -0700

    perf responsible in Reset - ARMY

--- src/pages/Signup/Reset.js
+++ src/pages/Signup/Reset.js
@@ -1,30 +1,33 @@
 import { useNavigate } from "react-router-dom";
 import MonkeyBtn from "../../components/MonkeyBtn";
+import { useState } from "react";
 const Reset = () => {
   const navigator = useNavigate();
+  const [value, setValue] = useState("");
+  const handleReset = () => {
+    if (!value.trim()) return;
+    navigator("/otp");
+  };
   return (
     <>
-      <div className="bg-white px-8 pt-9 pb-10 text-center h-[812px] content-between">
+      <div className="w-full flex flex-col justify-center pt-9 px-10 text-center">
         <div className="text-center text-3xl text-[#4A4B4D]">
           Reset Password
         </div>
         <div className="pt-3 text-[#7C7D7E] text-sm">
-          Please enter your email to receive a<br></br>link to create a new
-          password via email
+          Please enter your email to receive a link to create a new password via
+          email
         </div>
         <div className="pt-16">
           <input
             className="w-full rounded-full bg-[#F2F2F2] px-8 py-3 text-sm h-14"
             placeholder="Email"
-          ></input>
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
         </div>
 
-        <div
-          className="w-full py-8 cursor-pointer"
-          onClick={() => {
-            navigator("/otp");
-          }}
-        >
+        <div className="w-full py-8 cursor-pointer" onClick={handleReset}>
           <MonkeyBtn bgcolor={"#FC6011"} title={"Send"} />
         </div>
       </div>
