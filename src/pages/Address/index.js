4f1eea7bbc4ceb72358341376a3e87d9b091ea92
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-06-28 10:40:08 +0100

    fix address by CKS

--- src/pages/Address/index.js
+++ src/pages/Address/index.js
@@ -1,4 +1,23 @@
+import previous from "./img/previous.svg";
+import map from "./img/map.svg";
+
 const Address = () => {
-  return <>Address</>;
+  return (
+    <>
+      <div className="">
+        <div className="py-8 px-6">
+          <div className="flex justify-start items-center gap-5">
+            <div>
+              <img src={previous} alt="No image" />
+            </div>
+            <p className="text-2xl text-[#4A4B4D]">Change Address</p>
+          </div>
+        </div>
+        <div className="bg-zinc-600 h-[520px] px-3 mt-10">
+          <img className=" h-full scale-150" src={map} alt="no map" />
+        </div>
+      </div>
+    </>
+  );
 };
 export default Address;
