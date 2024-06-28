d8e17b9d53049c69ae0d230cad587bf3d11d0add
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-06-28 09:46:48 +0100

    perf offerItem-YJU

--- src/components/Offers/OffersComponent.js
+++ src/components/Offers/OffersComponent.js
@@ -1,21 +1,22 @@
-
 import RatingStar from "../../pages/Offers/images/rateStar.svg";
-const OffersComponent = ({description,img}) => {
+const OffersComponent = ({ description, img }) => {
   return (
-    <div className="w-full mt-[22px]">
+    <div className="w-full xss:mt-[22px] mt-[11px]">
       <img src={img} className="w-full" />
-      <div className="ml-[26px]">
-        <div className="text-base text-[#4A4B4D] font-bold mt-[7px] ">
-            {description}
+      <div className="ml-[26px] flex flex-row xss:flex-col items-center xss:items-start">
+        <div className="xss:text-base text-[#4A4B4D] font-bold xss:mt-[7px] mt-[2px] text-[11px] mr-3">
+          {description}
         </div>
-        <div className="flex flex-row items-center mb-[5px]">
+        <div className="flex flex-row items-center mb-[2px] xss:mb-[5px]">
           <div className="text-[#FC6011] text-[11px] flex flex-row mr-[5px]">
             <img src={RatingStar} className="mr-1" />
             4.9
           </div>
-          <div className="text-[#B6B7B7] text-xs ">(124 ratings) Café</div>
-          <div className="text-[#FC6011] text-base mb-[6px]">.</div>
-          <div className="text-[#B6B7B7] text-xs">Western Food</div>
+          <div className="hidden xss:block xss:flex flex-row items-center">
+            <div className="text-[#B6B7B7] text-xs ">(124 ratings) Café</div>
+            <div className="text-[#FC6011] text-base mb-[6px]">.</div>
+            <div className="text-[#B6B7B7] text-xs">Western Food</div>
+          </div>
         </div>
       </div>
     </div>
