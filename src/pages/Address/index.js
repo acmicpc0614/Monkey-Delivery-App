4a87673afdf455e375063b6eb43fe43aec53509f
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-06-28 15:52:26 +0100

    improve address by cks

--- src/pages/Address/index.js
+++ src/pages/Address/index.js
@@ -1,11 +1,25 @@
 import previous from "./img/previous.svg";
 import map from "./img/map.svg";
+import star from "./img/star.svg";
+import forward from "./img/forward.svg";
+import target from "./img/position.svg";
+import { useState } from "react";
 
 const Address = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const changePosition = (e) => {
+    var scroll = document.getElementById("scroll");
+    setPosition(() => {
+      return {
+        x: e.clientX - 180,
+        y: e.clientY - 69,
+      };
+    });
+  };
   return (
     <>
-      <div className="">
-        <div className="py-8 px-6">
+      <div className="w-full ">
+        <div className="py-8 px-6 ">
           <div className="flex justify-start items-center gap-5">
             <div>
               <img src={previous} alt="No image" />
@@ -13,8 +27,43 @@ const Address = () => {
             <p className="text-2xl text-[#4A4B4D]">Change Address</p>
           </div>
         </div>
-        <div className="bg-zinc-600 h-[520px] px-3 mt-10">
-          <img className=" h-full scale-150" src={map} alt="no map" />
+
+        <div
+          className="w-full overflow-x-scroll h-[520px] bg-slate-400 relate text-center"
+          onClick={changePosition}
+          id="scroll"
+        >
+          <img src={map} alt="No image" className="scale-[4] self-center" />
+          <div
+            className="absolute"
+            style={{ left: `${position.x}px`, top: `${position.y}px` }}
+          >
+            <img
+              src={target}
+              // className="translate-x-[-50%] translate-y-[-130%]"
+            />
+          </div>
+        </div>
+
+        <div className="px-5">
+          <div className="bg-[#F2F2F2] flex justify-start items-center rounded-full w-full h-[45px] my-5">
+            <div className="px-5 text-center text-xl text-[#4a4b4d]">
+              <i className="fa fa-search font-sans"></i>
+            </div>
+            <div className="">
+              <input
+                className=" bg-transparent focus:outline-none"
+                placeholder="Search food"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <img className="" src={star} alt="No image" />
+            <p className="flex-auto text-left text-[#4A4B4D] px-3">
+              Choose a saved place
+            </p>
+            <img className="" src={forward} alt="No image" />
+          </div>
         </div>
       </div>
     </>
